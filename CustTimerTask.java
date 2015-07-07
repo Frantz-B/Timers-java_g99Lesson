277edb64b7c1dd5c151ce724f83d12b4a2f6e51d
Frantz-B <Frantz.lesly@gmail.com>
2015-07-07 11:59:22 -0400

    Changed date Formatting -thx g99

--- CustTimerTask.java
+++ CustTimerTask.java
@@ -1,5 +1,6 @@
 import java.util.TimerTask;
 import java.util.Date;
+import java.text.SimpleDateFormat;
 
 //This is a simple class that uses the run() method of Threads when the task is invoked. 
 //The String strObject will help us identify which task is running
@@ -8,6 +9,7 @@ class CustTimerTask extends TimerTask{
 	//Not sure how run method is automatically executed tho.. Thought it was part of the Object class but didn't see it.
 
 	String strObject;
+	String strDateFormat = "hh:mm:ss a dd-MMM-yyyy";
 
 	public CustTimerTask (String strObject){
 		this.strObject = strObject;
@@ -16,6 +18,8 @@ class CustTimerTask extends TimerTask{
 	public void run(){
 		Date objDate = new Date(); //Adding to get timestamp feature ;-)
 		//is there a diff if I put the date variable as part of the class varible?
-		System.out.println("Name of task being run is: "+ strObject + "- "+ objDate.toString());
+		SimpleDateFormat objSDF = new SimpleDateFormat(strDateFormat);
+		//Interesting: missing format string 'strDateFormat' caused same date to be printed over & over
+		System.out.println("Name of task being run is: "+ strObject + "- "+objSDF.format(objDate));
 	}
 }
